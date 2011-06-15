a6647077d7b10ddf4947939682ae68ae63ee755a
Simon Koelsch <echox@echox.de>
2011-06-15 11:50:13 +0200

    extracetd some jasmine sh

---
+++ test/specs.js
@@ -0,0 +1,20 @@
+require.paths.push("./lib");
+require.paths.push("./specs");
+var jasmine = require('jasmine');
+var sys = require('sys');
+
+for(var key in jasmine) {
+  global[key] = jasmine[key];
+}
+
+jasmine.isVerbose = false;
+jasmine.showColors = true;
+process.argv.forEach(function(arg){
+  switch(arg) {
+  case '--color': jasmine.showColors = true; break;
+  case '--noColor': jasmine.showColors = false; break;
+  case '--verbose': jasmine.isVerbose = true; break;
+  }
+});
+
+jasmine.requireAllSpecFiles(__dirname + "/spec")
